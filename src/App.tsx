329eb5bc54b33ba1449fc8c8f4adfc92bacfd31b
satorukomatsu <sk.comiu@gmail.com>
2025-05-11 10:58:19 +0900

    映画リストをコンポーネントに切り出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,14 @@
 import Header from './components/Header'
 import SearchFormContent from './components/SearchFormContent'
+import MovieListContents from './components/MovieListContents'
 import './App.css'
-import mockData from './mockData.json'
-import genresData from './genresData.json'
-
-interface Genre {
-  id: number
-  name: string
-}
 
 function App() {
   return (
     <>
       <Header title='Search Movie App'/>
       <SearchFormContent/>
-      <div className='movieListContents'>
-        {mockData.results.map((result) => {
-          return (
-            <div key={result.id} className='movieDetail'>
-              <h3>{result.title}</h3>
-              <img className='movieImg' src={`https://image.tmdb.org/t/p/original${result.poster_path}`} alt={result.title}/>
-              <p>
-                ジャンル: {
-                  result.genre_ids.length ?
-                    result.genre_ids.map((genreId, index) => {
-                      const genre = genresData.genres.find((genre: Genre) => {
-                        return genre.id === genreId
-                      })
-                      return (
-                        <span key={genre ? genre.id : ''}>
-                          {genre ? genre.name : ''}
-                          {result.genre_ids.length - 1 > index ? ', ' : ''}
-                        </span>
-                      )
-                    }) :
-                    '情報なし'
-                }
-              </p>
-              <p>
-                リリース: {
-                  result.release_date ?
-                    new Date(result.release_date).toLocaleDateString('ja-JP', {
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric'
-                    }) :
-                    '情報なし'
-                }
-              </p>
-            </div>
-          )
-        })}
-      </div>
+      <MovieListContents/>
       <div>
         <button>Load More</button>
       </div>
---
+++ src/components/MovieListContents.tsx
@@ -0,0 +1,52 @@
+import mockData from '../mockData.json'
+import genresData from '../genresData.json'
+
+interface Genre {
+  id: number
+  name: string
+}
+
+function MovieListContents () {
+  return (
+    <div className='movieListContents'>
+      {mockData.results.map((result) => {
+        return (
+          <div key={result.id} className='movieDetail'>
+            <h3>{result.title}</h3>
+            <img className='movieImg' src={`https://image.tmdb.org/t/p/original${result.poster_path}`} alt={result.title}/>
+            <p>
+              ジャンル: {
+                result.genre_ids.length ?
+                  result.genre_ids.map((genreId, index) => {
+                    const genre = genresData.genres.find((genre: Genre) => {
+                      return genre.id === genreId
+                    })
+                    return (
+                      <span key={genre ? genre.id : ''}>
+                        {genre ? genre.name : ''}
+                        {result.genre_ids.length - 1 > index ? ', ' : ''}
+                      </span>
+                    )
+                  }) :
+                  '情報なし'
+              }
+            </p>
+            <p>
+              リリース: {
+                result.release_date ?
+                  new Date(result.release_date).toLocaleDateString('ja-JP', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                  }) :
+                  '情報なし'
+              }
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default MovieListContents
