3f4cbf2e30290e2871a2f47d727160140e240a5a
satorukomatsu <sk.comiu@gmail.com>
2025-05-11 11:20:00 +0900

    検索文字と年をstateで制御

--- src/components/SearchFormContent.tsx
+++ src/components/SearchFormContent.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+
 function SearchFormContent () {
+  const [keyword, setKeyword] = useState('')
+  const [selectedYear, setSelectedYear] = useState('')
+
   const searchYears = ['2020', '2021', '2022', '2023', '2024']
 
   return (
@@ -8,13 +13,18 @@ function SearchFormContent () {
         <input
           type='text'
           id='keyword'
-          value=''
+          value={keyword}
           placeholder='Please input keyword to search for'
+          onChange={(e) => { setKeyword(e.target.value) }}
         />
       </div>
       <div className='formGroup'>
         <label htmlFor='releaseYear'>Release Year</label>
-        <select id='releaseYear'>
+        <select
+          id='releaseYear'
+          value={selectedYear}
+          onChange={(e) => { setSelectedYear(e.target.value) }}
+        >
           <option value=''>---Please select year---</option>
           {searchYears.map((year) => {
             return <option key={year} value={year}>{year}</option>
